a48fb9f15ca39eaadec04f35c76ba90d83922b2c
BrunoADPaixao <bruno.amordivino@gmail.com>
2020-07-22 11:03:04 -0300

    First commit - Creating Buttons and Display

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native'
+import  Button from './src/components/Button'
+import Display from './src/components/Display'
+
+export default class App extends Component {
+  state = {
+    displayValue: '0'
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <Text style={styles.buttons}>
+          <Button label='AC' />
+          <Button label='/' />
+          <Button label='7' />
+          <Button label='8' />
+          <Button label='9' />
+          <Button label='*' />
+          <Button label='4' />
+          <Button label='5' />
+          <Button label='6' />
+          <Button label='-' />
+          <Button label='1' />
+          <Button label='2' />
+          <Button label='3' />
+          <Button label='+' />
+          <Button label='0' />
+          <Button label='.' />
+          <Button label='=' />
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+})
