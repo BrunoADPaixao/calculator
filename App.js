b151407509a6c169187501d55acb6d1d73d157b5
BrunoADPaixao <bruno.amordivino@gmail.com>
2020-07-23 12:18:11 -0300

    Finishing Calculator

--- App.js
+++ App.js
@@ -19,12 +19,14 @@ export default class App extends Component {
   state = { ...initialState }
 
   addDigit = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay 
+      && this.state.displayValue.includes('.')) {
       return
     }
 
-    const clearDisplay = this.state.displayValue === '0'
-      || this.state.clearDisplay
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false })
@@ -42,7 +44,26 @@ export default class App extends Component {
   }
 
   setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`, 
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
   render() {
